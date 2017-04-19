8041733e44d5ec604b9a327fe17525272a8a08e2
Nathan Rouxelin <nrouxelin@gmail.com>
2017-04-19 19:20:39 +0200

    CSS

--- www/index.php
+++ www/index.php
@@ -2,6 +2,7 @@
 
 define("ROOT", realpath(__dir__."/.."));
 define("DIR_IMG", 'http://'.$_SERVER['SERVER_NAME'].str_replace('index.php','',$_SERVER["PHP_SELF"])."images/");
+define("DIR_STYLES", 'http://'.$_SERVER['SERVER_NAME'].str_replace('index.php','',$_SERVER["PHP_SELF"])."styles/");
 
 require_once(ROOT."/app/noyau/Noyau.php");
 Noyau::lancer();
